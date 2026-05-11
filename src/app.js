c86512c18e469c482bd474a6ed2de6b7183144ef
Natalia Stiopu <stiopuaurora@gmail.com>
2026-05-11 18:51:05 +0000

    Generador de Dominio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-import "bootstrap";
-import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ["the", "our", "that", "best", "top"];
+  let adj = ["great", "big", "fast", "smart", "cool"];
+  let noun = ["jogger", "racoon", "rocket", "planet", "market", "vision", "welcome", "telecom"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let h = 0; h < adj.length; h++) {
+      for (let k = 0; k < noun.length; k++) {
+
+        let word = noun[k];
+
+        if (word.endsWith("com")) {
+          word = word.slice(0, -3);
+        } else if (word.endsWith("come")) {
+          word = word.slice(0, -4);
+        }
+
+        console.log(pronoun[i] + adj[h] + word + ".com");
+
+      }
+    }
+  }
+
